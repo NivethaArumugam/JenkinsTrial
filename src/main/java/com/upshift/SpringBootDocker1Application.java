ea455b17123be6fbd34da09cd0a7df1200fe32e3
NivethaArumugam <77378645+NivethaArumugam@users.noreply.github.com>
2022-03-03 20:57:44 +0530

    Update SpringBootDocker1Application.java

--- src/main/java/com/upshift/SpringBootDocker1Application.java
+++ src/main/java/com/upshift/SpringBootDocker1Application.java
@@ -12,12 +12,12 @@ public class SpringBootDocker1Application {
 
     @GetMapping("/")
     public String index(final Model model) {
-      //  model.addAttribute("title", "Docker + Spring Boot");
-        //model.addAttribute("msg", "Welcome to the docker container!");
+      System.out.println("This will render a page");
         return "index";
     }
 
 	public static void main(String[] args) {
+		System.out.println("This is a simple project");
 		SpringApplication.run(SpringBootDocker1Application.class, args);
 	}
 
